b670af971c212116b4ba98fe020698d23cf94b34
damao <deardamao@gmail.com>
2017-01-31 14:08:35 +0800

    add handle code

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-import MySQLdb
+# coding:utf-8
 from flask import Flask, g, request
+import hashlib
 
 app = Flask(__name__)
 app.debug = True
-
-# from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
-#     MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
-# )
-
-# @app.before_request
-# def before_request():
-#     g.db = MySQLdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS,
-#                            MYSQL_DB, port=int(MYSQL_PORT))
 
 @app.teardown_request
 def teardown_request(exception):
     if hasattr(g, 'db'): g.db.close()
 
 @app.route('/')
-def hello():
-	s = GET()
-    return s
-
-# @app.route('/demo', methods=['GET', 'POST'])
-# def greeting():
-#     html = ''
-
-#     if request.method == 'POST':
-#         c = g.db.cursor()
-#         c.execute("insert into demo(text) values(%s)", (request.form['text']))
-
-#     html += """
-#     <form action="" method="post">
-#         <div><textarea cols="40" name="text"></textarea></div>
-#         <div><input type="submit" /></div>
-#     </form>
-#     """
-#     c = g.db.cursor()
-#     c.execute('select * from demo')
-#     msgs = list(c.fetchall())
-#     msgs.reverse()
-#     for row in msgs:
-#         html +=  '<p>' + row[-1] + '</p>'
-
-#     return html
-
-import hashlib
-# import web
-
 def GET():
     try:
         data = request.get_data()
@@ -64,10 +26,12 @@ def GET():
         sha1 = hashlib.sha1()
         map(sha1.update, list)
         hashcode = sha1.hexdigest()
-        print "handle/GET func: hashcode, signature: ", hashcode, signature
+        # print "handle/GET func: hashcode, signature: ", hashcode, signature
         if hashcode == signature:
             return echostr
         else:
             return ""
-    except Exception, Argument:
+    except Exception as Argument:
         return Argument
+
+# app.run()
