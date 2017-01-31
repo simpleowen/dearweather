ca4d1f66c423df606fdbe616fb885b003d2d5f75
damao <deardamao@gmail.com>
2017-01-31 14:34:20 +0800

    add handle code

--- main.py
+++ main.py
@@ -5,33 +5,28 @@ import hashlib
 app = Flask(__name__)
 app.debug = True
 
-@app.teardown_request
-def teardown_request(exception):
-    if hasattr(g, 'db'): g.db.close()
-
 @app.route('/')
 def GET():
-    try:
-        data = request.get_data()
-        if len(data) == 0:
-            return "hello, this is handle view"
-        signature = data.signature
-        timestamp = data.timestamp
-        nonce = data.nonce
-        echostr = data.echostr
-        token = "ichat" #请按照公众平台官网\基本配置中信息填写
-
-        list = [token, timestamp, nonce]
-        list.sort()
-        sha1 = hashlib.sha1()
-        map(sha1.update, list)
-        hashcode = sha1.hexdigest()
-        print "handle/GET func: hashcode, signature: ", hashcode, signature
-        if hashcode == signature:
-            return echostr
+	if request.method == 'GET':
+        if len(request.args) > 3:
+            temparr = []
+            token = "你的token"
+            signature = request.args["signature"]
+            timestamp = request.args["timestamp"]
+            nonce = request.args["nonce"]
+            echostr = request.args["echostr"]
+            temparr.append(token)
+            temparr.append(timestamp)
+            temparr.append(nonce)
+            temparr.sort()
+            newstr = "".join(temparr)
+            sha1str = hashlib.sha1(newstr)
+            temp = sha1str.hexdigest()
+            if signature == temp:
+                return echostr
+            else:
+                return "认证失败，不是微信服务器的请求！"
         else:
-            return ""
-    except Exception, Argument:
-        return Argument
+            return "你请求的方法是：" + request.method
 
 # app.run()
